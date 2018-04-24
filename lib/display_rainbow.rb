eda24f26915d0623baf6482c0cf9e1623730a737
Maria  Morris <mariasemailbox@gmail.com>
2018-04-24 01:22:46 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,4 +4,4 @@ def display_rainbow(roygbiv)
  puts "R: #{roygbiv[0]}, O: #{roygbiv[1]}, Y: #{roygbiv[2]}, G: #{roygbiv[3]}, B: #{roygbiv[4]}, I: #{roygbiv[5]}, V: #{roygbiv[6]}"
 end
 
-display_rainbow
+display_rainbow(5)
